7ec5e71d18a726e04c685ff0f0757c7e71966bcf
Alexander Lehning <alexander.lehning@tuhh.de>
2023-04-11 14:48:39 +0200

    chore: some modification to match coding guidelines

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,33 +1,28 @@
 <?php
+defined('TYPO3') or die();
 
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
-$_EXTKEY = 'aoe_ipauth';
-$backendConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class)->get('backend');
+(function ($extkey) {
+    $backendConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class)->get('backend');
 
-if ('BE' === TYPO3_MODE) {
-    // Do not show the IP records in the listing
-    $allowedTablesTs = '
+    if ('BE' === TYPO3_MODE) {
+        // Do not show the IP records in the listing
+        $allowedTablesTs = '
 		mod.web_list.deniedNewTables := addToList(tx_aoeipauth_domain_model_ip)
 		mod.web_list.hideTables := addToList(tx_aoeipauth_domain_model_ip)
 	';
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig($allowedTablesTs);
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig($allowedTablesTs);
 
-    // Hooks
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][$_EXTKEY] = \AOE\AoeIpauth\Hooks\Tcemain::class;
-} elseif ('FE' === TYPO3_MODE) {
-    $extensionConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-        \TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class
-    )->get($_EXTKEY);
-    $GLOBALS['TYPO3_CONF_VARS']['SVCONF']['auth']['setup']['FE_fetchUserIfNoSession'] =
-        isset($extensionConfiguration['fetchFeUserIfNoSession']) ? boolval($extensionConfiguration['fetchFeUserIfNoSession']) : 1;
-    unset($extensionConfiguration);
-}
+        // Hooks
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][$extkey] = \AOE\AoeIpauth\Hooks\Tcemain::class;
+    } elseif ('FE' === TYPO3_MODE) {
+        $extensionConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class)->get($extkey);
+        $GLOBALS['TYPO3_CONF_VARS']['SVCONF']['auth']['setup']['FE_fetchUserIfNoSession']
+            = isset($extensionConfiguration['fetchFeUserIfNoSession']) ? boolval($extensionConfiguration['fetchFeUserIfNoSession']) : 1;
+        unset($extensionConfiguration);
+    }
 
-// IP Authentication Service
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addService($_EXTKEY, 'auth', 'tx_aoeipauth_typo3_service_authentication',
-    array(
+    // IP Authentication Service
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addService($extkey, 'auth', 'tx_aoeipauth_typo3_service_authentication', array(
         'title' => 'IP Authentication',
         'description' => 'Authenticates against IP addresses and ranges.',
         'subtype' => 'authUserFE,getUserFE,getGroupsFE',
@@ -37,7 +32,6 @@ if ('BE' === TYPO3_MODE) {
         'quality' => 50,
         'os' => '',
         'exec' => '',
-        'classFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Classes/Typo3/Service/Authentication.php',
-        'className' => 'AOE\AoeIpauth\Typo3\Service\Authentication',
-    )
-);
+        'className' => \AOE\AoeIpauth\Typo3\Service\Authentication::class
+    ));
+})('aoe_ipauth');
--- ext_tables.php
+++ ext_tables.php
@@ -1,8 +1,5 @@
 <?php
-
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
+defined('TYPO3') or die();
 
 // IP
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
