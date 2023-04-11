000110c8d91178d4ba170b89abda731d503799d5
Alexander Lehning <alexander.lehning@tuhh.de>
2023-04-11 14:54:04 +0200

    fix: use EventListeners to fix frontend group handling

---
+++ Classes/EventListener/AddIpGroupToFrontendAuthentification.php
@@ -0,0 +1,29 @@
+<?php
+namespace AOE\AoeIpauth\EventListener;
+
+use TYPO3\CMS\Frontend\Authentication\ModifyResolvedFrontendGroupsEvent;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class AddIpGroupToFrontendAuthentification
+{
+    public function __invoke(ModifyResolvedFrontendGroupsEvent $event): void {
+        /** @var \TYPO3\CMS\Core\Http\NormalizedParams $normalizedParams */
+        $normalizedParams = $event->getRequest()->getAttribute('normalizedParams');
+        $requestIp = $normalizedParams->getRemoteAddress();
+        $ipGroups = $this->getFeEntityService()->findAllGroupsWithIpAuthentication($requestIp);
+        $event->setGroups($event->getGroups() + $ipGroups);
+    }
+    
+    /**
+     * @return \AOE\AoeIpauth\Domain\Service\FeEntityService
+     */
+    protected function getFeEntityService()
+    {
+        if (null === $this->feEntityService) {
+            $this->feEntityService = GeneralUtility::makeInstance(\AOE\AoeIpauth\Domain\Service\FeEntityService::class);
+        }
+        return $this->feEntityService;
+    }
+    
+}
+
